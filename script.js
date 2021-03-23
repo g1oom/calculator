ef7911fdd72cea5db8095a55b8b48c53a903bccc
g1oom <albertowj2001@gmail.com>
2021-03-23 18:38:39 +0800

    Grouped buttons, added display, and started creating functions to change display

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
-function add(a,b) {
-    return a+b;
+function add(a, b) {
+    return a + b;
 }
 
-function subtract(a,b) {
-    return a-b;
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply(a,b) {
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(a,b) {
-    if(b==0) {
-        return false
+function divide(a, b) {
+    if (b == 0) {
+        return "no u";
     } else {
-        return a/b;
+        return a / b;
     }
 }
 
-function operate(num1,operator,num2) {
-    if(operator=='+') {
-        return add(num1,num2);
-    } else if(operator=='-') {
-        return subtract(num1,num2);
-    } else if(operator=='*') {
-        return multiply(num1,num2);
-    } else if(operator=='/') {
-        return divide(num1,num2);
+function operate(num1, operator, num2) {
+    if (operator == '+') {
+        return add(num1, num2);
+    } else if (operator == '-') {
+        return subtract(num1, num2);
+    } else if (operator == '*') {
+        return multiply(num1, num2);
+    } else if (operator == '/') {
+        return divide(num1, num2);
+    } else {
+        return "invalid";
     }
 }
+
+let numbers = document.querySelector('.calculator-button .number');
+numbers.addEventListener('click', function(event) {
+    console.log(event.target);
+});
+
+/*
+let numbers = document.querySelectorAll('.number');
+let operators = document.querySelectorAll('.operator');
+let display = document.querySelector('.display');
+
+numbers.forEach(this.onclick = function () {
+    display.innerHTML += this.innerHTML;
+});
+*/
