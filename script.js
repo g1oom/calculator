65a44fd1f60c1597612aa5096e7864ef69662e3f
g1oom <albertowj2001@gmail.com>
2021-04-30 23:02:50 +0800

    Fixed how to deal with error result, and added decimal point

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-    return a * b;
+    return +(a * b).toFixed(11);
 }
 
 function divide(a, b) {
@@ -39,11 +39,6 @@ function operate(num1, operator, num2) {
         return "Infinity";
     }
 
-    // check for calculations involving error
-    if (num1 == "Error") {
-        return "Error";
-    }
-
     num1 = Number(num1);
     num2 = Number(num2);
     if (operator == '+') {
@@ -60,59 +55,95 @@ function operate(num1, operator, num2) {
 function update_display_num(num_input) {
     let current_num = document.querySelector('.calculator-display').innerText;
 
-    // if last key pressed was operator, refresh display
-    if ((last_key != null) && (last_key.match(/[\+\-\*\/\=]/))) {
-        current_num = num_input;
-    }
-
-    // numbers with more than one digit or non decimals cannot start with 0
-    else if (current_num == '0') {
-        if (num_input != '0') {
+    if (current_num != "Error") {
+        // if last key pressed was operator, refresh display
+        if ((last_key != null) && (last_key.match(/[\+\-\*\/\=]/))) {
             current_num = num_input;
         }
-    }
-    else {
-        current_num += num_input;
-    }
 
-    document.querySelector('.calculator-display').innerText = current_num;
-    last_key = num_input;
+        // numbers with more than one digit or non decimals cannot start with 0
+        else if (current_num == '0') {
+            if (num_input != '0') {
+                current_num = num_input;
+            }
+        }
+        else {
+            current_num += num_input;
+        }
+
+        document.querySelector('.calculator-display').innerText = current_num;
+        last_key = num_input;
+    }
 }
 
 function update_display_op(op_input) {
     let current_num = document.querySelector('.calculator-display').innerText;
 
-    if (last_key == null) {
-        // if first button pressed is operator, assume first number is 0
-        saved_num = current_num.toString();
-    } else {
-        // store previous results to saved_num and show answer if operate can be called
-        if (!last_key.match(/[\+\-\*\/]/)) {
-            if (saved_num == null) {
-                saved_num = current_num.toString(); // saved_num is stored in string format to account for Infinity and Error
-            } else {
-                saved_num = operate(saved_num, saved_op, current_num);
-                document.querySelector('.calculator-display').innerText = saved_num;
+    if (current_num != "Error") {
+        if (last_key == null) {
+            // if first button pressed is operator, assume first number is 0
+            saved_num = current_num.toString();
+        } else {
+            // store previous results to saved_num and show answer if operate can be called
+            if (!last_key.match(/[\+\-\*\/]/)) {
+                if (saved_num == null) {
+                    saved_num = current_num.toString(); // saved_num is stored in string format to account for Infinity and Error
+                } else {
+                    saved_num = operate(saved_num, saved_op, current_num);
+                    document.querySelector('.calculator-display').innerText = saved_num;
+                }
             }
         }
-    }
+        
+        // if result is Error, reset saved_num and saved_op
+        if (saved_num == "Error") {
+            saved_num = null;
+            saved_op = null;
+        } else {
+            saved_op = op_input;
+        }
 
-    saved_op = op_input;
-    last_key = op_input;
+        last_key = op_input;
+    }
+}
+
+function update_display_decimal() {
+    let current_num = document.querySelector('.calculator-display').innerText;
+    if (current_num != "Error") {
+        // if last key pressed was operator, refresh display
+        if ((last_key != null) && (last_key.match(/[\+\-\*\/\=]/))) {
+            current_num = "0.";
+        } else {
+            // check if there's already one decimal place in number
+            let length = current_num.length;
+            let i = 0;
+            while ((i < length) && (current_num[i] != '.')) {
+                i++;
+            }
+            if (i == length) {
+                current_num += '.';
+            }
+        }
+
+        document.querySelector('.calculator-display').innerText = current_num;
+        last_key = '.';
+    }
 }
 
 function update_display_equal() {
     let current_num = document.querySelector('.calculator-display').innerText;
 
-    // check if operate can be called
-    if ((!isNaN(last_key)) && (saved_num != null)) {
-        document.querySelector('.calculator-display').innerText = operate(saved_num, saved_op, current_num);
-        // after last operation, refresh saved variables
-        saved_num = null;
-        saved_op = null;
+    if (current_num != "Error") {
+        // check if operate can be called
+        if ((!isNaN(last_key)) && (saved_num != null)) {
+            document.querySelector('.calculator-display').innerText = operate(saved_num, saved_op, current_num);
+            // after last operation, refresh saved variables
+            saved_num = null;
+            saved_op = null;
+        }
+        
+        last_key = '=';
     }
-
-    last_key = '=';
 }
 
 function update_display_clear() {
@@ -132,6 +163,11 @@ operators.forEach(operator => operator.addEventListener('click', function() {
     update_display_op(operator.innerText);
 }));
 
+let decimal = document.querySelector('.decimal');
+decimal.addEventListener('click', function() {
+    update_display_decimal();
+});
+
 let equal = document.querySelector('.equal');
 equal.addEventListener('click', function() {
     update_display_equal();
@@ -146,4 +182,4 @@ let saved_num = null;
 let saved_op = null;
 let last_key = null;
 
-// todo: when result is error, reset everything, but still display error instead of 0
+// todo: css time!
