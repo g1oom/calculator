f0a55db22bdba9a61f39600a68c1f77e701dd0bb
g1oom <albertowj2001@gmail.com>
2021-05-18 19:43:03 +0800

    Update calculator functions and interface
    - Add positive/negative button
    - Improve calculator interface for keyboard users

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function add(a, b) {
 }
 
 function subtract(a, b) {
-    // calculate number of decimal places of results
+    // fix recurring decimals for subtracting float values
     let dpA = 0;
     let dpB = 0;
     if (Math.floor(a) !== a) {
@@ -27,8 +27,8 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    if (b == 0) {
-        if (a == 0) {
+    if (b == 0 || b == -0) {
+        if (a == 0 || a == -0) {
             return "Error";
         }
         return "Infinity";
@@ -40,7 +40,7 @@ function divide(a, b) {
             let digitCount = answer.toString().split('.').join('').length;
             if (digitCount > 12) {
                 let dp = 12 - answer.toString().split('.')[0].length;
-                answer = answer.toFixed(dp);
+                answer = +answer.toFixed(dp);
             }
         }
         return answer;
@@ -51,7 +51,7 @@ function operate(num1, operator, num2) {
     // check for calculations involving infinity
     num2 = Number(num2);
     if (num1 == "Infinity") {
-        if (num2 == '0' && operator == '*') {
+        if ((num2 == '0' || num2 == "-0") && operator == '*') {
             return "Error";
         }
         return "Infinity";
@@ -74,7 +74,7 @@ function update_display_num(num_input) {
 
     if (current_num != "Error") {
         // if last key pressed was operator, refresh display
-        if ((last_key != null) && (last_key.match(/[\+\-\*\÷\/\=]/))) {
+        if ((last_key != null) && (last_key.match(/[+\-*÷/=]/))) {
             current_num = num_input;
         }
 
@@ -84,6 +84,14 @@ function update_display_num(num_input) {
                 current_num = num_input;
             }
         }
+
+        // negative zero
+        else if (current_num == "-0") {
+            if (num_input != '0') {
+                current_num = '-' + num_input;
+            }
+        }
+
         else {
             current_num += num_input;
         }
@@ -102,7 +110,7 @@ function update_display_op(op_input) {
             saved_num = current_num.toString();
         } else {
             // store previous results to saved_num and show answer if operate can be called
-            if (!last_key.match(/[\+\-\*\÷\/]/)) {
+            if (!last_key.match(/[+\-*÷/]/)) {
                 if (saved_num == null) {
                     saved_num = current_num.toString(); // saved_num is stored in string format to account for Infinity and Error
                 } else {
@@ -128,7 +136,7 @@ function update_display_decimal() {
     let current_num = document.querySelector('.calculator-display-inner').innerText;
     if (current_num != "Error") {
         // if last key pressed was operator, refresh display
-        if ((last_key != null) && (last_key.match(/[\+\-\*\÷\/\=]/))) {
+        if ((last_key != null) && (last_key.match(/[+\-*÷/=]/))) {
             current_num = "0.";
         } else {
             // check if there's already one decimal place in number
@@ -147,12 +155,31 @@ function update_display_decimal() {
     }
 }
 
+function update_display_plusminus() {
+    let current_num = document.querySelector('.calculator-display-inner').innerText;
+    if (current_num != "Error") {
+        // if last key pressed was operator, refresh display
+        if ((last_key != null) && (last_key.match(/[+\-*÷/=]/))) {
+            current_num = "-0";
+        } else {
+            if (current_num[0] == '-') {
+                current_num = current_num.substring(1);
+            } else {
+                current_num = '-' + current_num;
+            }
+        }
+
+        document.querySelector('.calculator-display-inner').innerText = current_num;
+        last_key = '_';
+    }
+}
+
 function update_display_equal() {
     let current_num = document.querySelector('.calculator-display-inner').innerText;
 
     if (current_num != "Error") {
         // check if operate can be called
-        if ((last_key.match(/[0-9\.]/)) && (saved_num != null)) {
+        if ((last_key.match(/[0-9._]/)) && (saved_num != null)) {
             document.querySelector('.calculator-display-inner').innerText = operate(saved_num, saved_op, current_num);
             // after last operation, refresh saved variables
             saved_num = null;
@@ -173,10 +200,16 @@ function update_display_clear() {
 function update_display_back() {
     let current_num = document.querySelector('.calculator-display-inner').innerText;
 
-    if ((last_key != null) && (last_key.match(/[0-9\.]/))) {
-        if (current_num.length <= 1) {
+    if ((last_key != null) && (last_key.match(/[0-9._]/))) {
+        if (current_num.length <= 1 || current_num == "-0") {
             current_num = '0';
-        } else {
+        }
+
+        else if (current_num.length == 2 && current_num[0] == '-') {
+            current_num = "-0";
+        }
+
+        else {
             current_num = current_num.slice(0, -1);
         }
         document.querySelector('.calculator-display-inner').innerText = current_num;
@@ -196,6 +229,11 @@ operators.forEach(operator => operator.addEventListener('click', function() {
 let decimal = document.querySelector('.decimal');
 decimal.addEventListener('click', function() {
     update_display_decimal();
+});
+
+let plusminus = document.querySelector('.plusminus');
+plusminus.addEventListener('click', function() {
+    update_display_plusminus();
 });
 
 let equal = document.querySelector('.equal');
@@ -224,30 +262,107 @@ document.addEventListener('keydown', function(event) {
         // numbers
         if (!isNaN(event.key)) {
             update_display_num(event.key);
+            numbers[`${event.key}`].classList.add("active");
         }
 
         // operators
         else if (event.key.match(/[\+\-\*\/]/)) {
             update_display_op(event.key);
+
+            // loop to find corresponding button
+            if (event.key === '/') {
+                document.getElementById("obelus").classList.add("active");
+            } else {
+                let k = event.key;
+                let i = 0;
+                let found = false;
+                while (i < 3 && !found) {
+                    if (k == operators[i].innerHTML) {
+                        operators[i].classList.add("active");
+                        found = true;
+                    }
+                    i++;
+                }
+            }
         }
 
         // decimal
         else if (event.key === '.') {
             update_display_decimal();
+            document.querySelector('.decimal').classList.add("active");
+        }
+        
+        // plusminus
+        else if (event.key === '_') {
+            update_display_plusminus();
+            document.querySelector('.plusminus').classList.add("active");
         }
 
         // equal
         else if (event.key === '=' || event.key === "Enter") {
             update_display_equal();
+            document.querySelector('.equal').classList.add("active");
         }
 
         // clear
         else if (event.key === 'c' || event.key === 'C') {
             update_display_clear();
+            document.querySelector('.clear').classList.add("active");
         }
         // back
         else if (event.key === "Backspace") {
             update_display_back();
+            document.querySelector('.back').classList.add("active");
         }
+    }
+});
+
+// CSS purposes: remove darker button border when keyup
+document.addEventListener('keyup', function(event) {
+    // numbers
+    if (!isNaN(event.key)) {
+        numbers[`${event.key}`].classList.remove("active");
+    }
+
+    // operators
+    else if (event.key.match(/[\+\-\*\/]/)) {
+        if (event.key === '/') {
+            document.getElementById("obelus").classList.remove("active");
+        } else {
+            let k = event.key;
+            let i = 0;
+            let found = false;
+            while (i < 3 && !found) {
+                if (k == operators[i].innerHTML) {
+                    operators[i].classList.remove("active");
+                    found = true;
+                }
+                i++;
+            }
+        }
+    }
+
+    // decimal
+    else if (event.key === '.') {
+        document.querySelector('.decimal').classList.remove("active");
+    }
+
+    // plusminus
+    else if (event.key === '_') {
+        document.querySelector('.plusminus').classList.remove("active");
+    }
+
+    // equal
+    else if (event.key === '=' || event.key === "Enter") {
+        document.querySelector('.equal').classList.remove("active");
+    }
+
+    // clear
+    else if (event.key === 'c' || event.key === 'C') {
+        document.querySelector('.clear').classList.remove("active");
+    }
+    // back
+    else if (event.key === "Backspace") {
+        document.querySelector('.back').classList.remove("active");
     }
 });
